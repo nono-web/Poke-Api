e3537c442419dd9615ef242ea1b63e2ee131c544
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2023-04-25 08:00:41 +0200

    mise a jours

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -9,58 +9,67 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const ContainerDesc = styled.div`
+  background-color: #ffffffa9;
+  border-radius: 2rem;
+`;
+
 const Desc = styled.p`
   margin: 1rem;
-  color: red;
+  color: #2503fdfd;
   font-weight: bold;
   font-size: 2rem;
-  text-shadow: 4px 4px 2px rgb(243, 169, 8);
   text-align: center;
   ${desktop({ fontSize: '4rem' })}
 `;
 
 const Image = styled.img`
-  width: 20rem;
-  height: 16rem;
+  width: 25rem;
+  height: 22rem;
   margin: 1rem;
 `;
 
 const ContainerButton = styled.div`
   display: flex;
-  flex-direction: column;
+
   align-items: center;
 `;
 
-const Button = styled.button `
+const Button = styled.button`
   border: 0.1rem solid black;
   border-radius: 2rem;
   margin: 1rem;
-  width: 5rem;
-  height: 2rem;
+  width: 8rem;
+  height: 3rem;
   background-color: rgb(0, 195, 255);
   font-weight: bold;
   &:nth-child(1) {
     &:hover {
-      background-color: rgba(255, 99, 71, 0.452);
-}
-    }
-  &:nth-child(2) {
-    &:hover {
-      background-color: rgba(71, 255, 141, 0.452);
+      background-color: rgb(252, 2, 2);
     }
   }
-`
+  &:nth-child(2) {
+    &:hover {
+      background-color: rgb(71, 255, 141);
+    }
+  }
+`;
 
 const Pokemon = () => {
   const [poke, setPoke] = useState([]);
   const [pokeid, setPokeid] = useState(1);
 
-  const getPokeApi = (id) => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+  const getPokeApi = async (id) => {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
       console.log(response.data);
       setPoke(response.data);
       setPokeid(id);
-    });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -69,25 +78,21 @@ const Pokemon = () => {
 
   return (
     <Container>
-      <Desc>Hello Trainer, you just captured {poke.name}</Desc>
-      <Desc>The N° {poke.id} in the Pokedex </Desc>
+      <ContainerDesc>
+        <Desc>Hello Trainer, you just captured {poke.name}</Desc>
+        <Desc>The N° {poke.id} in the Pokedex </Desc>
+      </ContainerDesc>
       <Image
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${poke.id}.svg`}
         alt={poke.name}
       />
       <ContainerButton>
-      <Button
-        type="button"
-        onClick={() => getPokeApi(pokeid - 1)}
-      >
-        Prev
-      </Button>
-      <Button
-        type="button"
-        onClick={() => getPokeApi(pokeid + 1)}
-      >
-        Next
-      </Button>
+        <Button type="button" onClick={() => getPokeApi(pokeid - 1)}>
+          Prev
+        </Button>
+        <Button type="button" onClick={() => getPokeApi(pokeid + 1)}>
+          Next
+        </Button>
       </ContainerButton>
     </Container>
   );
